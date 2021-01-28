17a816244b07ffd768a63976c20c6538dd079c45
shindong96 <70707629+shindong96@users.noreply.github.com>
2021-01-28 20:46:37 +0900

    32. 선택 정렬

---
+++ 32. 선택정렬.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+32. 선택정렬
+N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+정렬하는 방법은 선택정렬입니다.
+▣ 입력설명
+첫 번째 줄에 자연수 N(1<=N<=100)이 주어집니다.
+두 번째 줄에 N개의 자연수가 공백을 사이에 두고 입력됩니다. 각 자연수는 정수형 범위 안에
+있습니다.
+▣ 출력설명
+오름차순으로 정렬된 수열을 출력합니다.*/
+
+int main() {
+	int n;
+	cin >> n;
+	int* a = new int[n];
+	for (int i = 0; i < n; i++) cin >> a[i];
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (a[i] > a[j]) {
+				int tmp = a[j];
+				a[j] = a[i];
+				a[i] = tmp;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+	delete[] a;
+	return 0;
+}                        //최소값을 안쪽 for문에서 찾아서 한 번 만 swap을 할 수 있다.
+
+/*
+모범 답안
+
+#include<stdio.h>
+int main() {
+	freopen("input.txt", "rt", stdin);
+	int a[101], n, tmp, idx, i, j;
+	scanf("%d", &n);
+	for(i=0; i<n; i++){
+		scanf("%d", &a[i]);
+	}
+	for(i=0; i<n-1; i++){
+		idx=i;
+		for(j=i+1; j<n; j++){
+			if(a[j]<a[idx]) idx=j;
+		}
+		tmp=a[i];
+		a[i]=a[idx];
+		a[idx]=tmp;
+	}
+	for(i=0; i<n; i++){
+		printf("%d ", a[i]);
+	}
+	return 0;
+}
+*/
