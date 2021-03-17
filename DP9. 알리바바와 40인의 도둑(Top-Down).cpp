9bda429ea90d5e5c0b52c43aad2327f2d5f31ac9
shindong96 <70707629+shindong96@users.noreply.github.com>
2021-03-17 20:34:44 +0900

    DP9. 알리바바와 40인의 도둑(Top-Down)

---
+++ DP9. 알리바바와 40인의 도둑(Top-Down).cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int n;
+int** a;
+bool** flag;
+
+int run(int x, int y) {
+	int leftside;
+	int topside;
+	if (x == 0) {
+		if (y == 0) {
+			leftside = 0;
+			topside = 0;
+		}
+		else {
+			leftside = !flag[x][y - 1] ? a[x][y - 1]:run(x, y - 1);
+			topside = 10000000;
+		}
+	}
+	else if (y == 0) {
+		leftside = 10000000;
+		topside = !flag[x - 1][y] ? a[x - 1][y] : run(x - 1, y);
+	}
+	else {
+		leftside = !flag[x][y - 1] ? a[x][y - 1] : run(x, y - 1);
+		topside = !flag[x - 1][y] ? a[x - 1][y] : run(x - 1, y);
+	}
+	a[x][y] += (leftside < topside) ? leftside : topside;
+	flag[x][y] = false;
+	return a[x][y];
+}
+
+int main() {
+	cin >> n;
+	a = new int* [n];
+	flag = new bool* [n];
+	for (int i = 0; i < n; i++) {
+		a[i] = new int[n];
+		flag[i] = new bool[n];
+		for (int j = 0; j < n; j++) cin >> a[i][j];
+	}
+	cout << run(n - 1, n - 1);
+}
